b1af8aed4d755316fbea525c08635591f052e6a0
Darmawan Abinugroho <56748343+darmawanabi@users.noreply.github.com>
2019-11-14 16:44:22 +0700

    Create week2_dictionary.py

---
+++ darmawanabi/week2_dictionary.py
@@ -0,0 +1,13 @@
+class Orang:
+    def __init__(__args__):
+        pass
+
+plants = {}
+plants["apel"] = 10
+plants["rambutan"] = 30
+plants[1] = "rambutan"
+
+aku = Orang()
+plants[aku] = 1
+
+print(plants)
