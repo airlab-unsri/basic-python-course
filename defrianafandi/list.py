5c5a5a23cb7bcf3260a5dc2bdaa722e3586d9b47
Defrian Afandi <55933413+defrianafandi@users.noreply.github.com>
2019-11-15 19:09:01 +0700

    create list

---
+++ defrianafandi/list.py
@@ -0,0 +1,13 @@
+my_list = []
+
+my_list.append(1)
+my_list.append(3)
+my_list.append(5)
+
+my_list.append(("Defrian", "Afandi", "Hasan"))
+
+colors = {}
+colors["Biru"] = "Blue"
+my_list.append(colors)
+
+print(my_list)
