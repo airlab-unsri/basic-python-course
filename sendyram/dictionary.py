9cf4fed2314d112285dcbd0077e86faefadf8eb0
Sendy Ramadhan <sendyramadhan14@gmail.com>
2019-11-19 21:07:13 +0700

    Create dictionary.py

---
+++ sendyram/dictionary.py
@@ -0,0 +1,14 @@
+class kucing:
+    def __init__(__args__):
+        pass
+
+
+animal = {}
+animal["lion"] = 10
+animal["whale"] = 30
+animal[1] = "whale"
+
+you = kucing()
+animal[you] = 1
+
+print(animal)
