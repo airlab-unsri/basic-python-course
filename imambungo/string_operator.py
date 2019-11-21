7ffd32d2477836c23bca7f768fb715449ac05bf3
M. Imam Pratama <imambungo@gmail.com>
2019-11-21 08:24:12 +0700

    finished week 1 assignment
     - exception
     - namespace
     - string operator

---
+++ imambungo/string_operator.py
@@ -0,0 +1,25 @@
+a_string = "this is a string"
+
+print('\nconcat')
+print('look, ' + a_string)
+
+print('\nslicing')
+print(a_string[2:4])
+
+print('\nlower()')
+print(a_string.lower())
+
+print('\nupper()')
+print(a_string.upper())
+
+print('\nlen()')
+print(len(a_string))
+
+print('\nstrip()')
+print(a_string.strip('gt'))
+
+print('\nreplace()')
+print(a_string.replace('this', 'that'))
+
+print('\nsplit()')
+print(a_string.split(' '))
